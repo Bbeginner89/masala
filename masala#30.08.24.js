88cc8d7126adf0e38833b1ca73623a5bc53248ed
Bbeginner89 <112170134+Bbeginner89@users.noreply.github.com>
2024-08-30 21:13:22 +0900

    Update masala#30.08.24.js

--- masala#30.08.24.js
+++ masala#30.08.24.js
@@ -1,1 +1,54 @@
 
+// *TODO #1 ushbu functionda user.ref.name nimaga teng va nega? console.log(user.ref.name); user.ref.name nimaga teng va nega? */
+/*function makeUser() {
+    return {
+      name: "John",
+      ref: this
+    };
+  }
+  
+  let user = makeUser();
+  
+  console.log(user.ref.name)*/
+
+
+// *TODO #2 conssole.logda nima chiqadi va nega shu code ni izohlab bering 
+/*let a = 10;
+increase(a)
+function increase(a) {
+  ++a;
+
+}
+
+console.log(a); */ 
+
+
+// *TODO #3 
+/**
+ * berilgan text stringdagi eng oxirgi elementni lengini toping masalan
+ * input: "Hello World"                  output: 5 
+ * input: "   fly me   to   the moon  "  output: 4
+ * input: "luffy is still joyboy"        output: 6
+ */
+
+/*let text = "   fly me   to   the moon  "
+function getLength(inputString) {
+    let arr = inputString.split(" ")
+    let last = arr.filter(function (value) {
+      return value.length>0
+    } 
+  )
+  return last[last.length-1].length        
+}
+
+console.log(getLength(text)) */ 
+
+
+// *TODO #4
+/**shunday function yozingki unga text berib number qiymat berilsa, shu texni uznuligi numberdan kattal bolsa  
+ * textni shu numbergacha kesib qolganiga ... belisini qoshisin.
+ masalan:
+ let srt="100 qator so'z"
+ let num=10
+  exampleFunction(str,num) //100 qator...
+ */
